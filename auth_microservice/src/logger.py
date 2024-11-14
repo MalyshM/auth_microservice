7df1e60a2c0773048bf406d09da33cd7802921d4
m.malysh <m.malysh@mail.ocas.local>
2024-11-14 16:36:03 +0500

    add logger

--- auth_microservice/src/logger.py
+++ auth_microservice/src/logger.py
@@ -1,1 +1,28 @@
-# TODO из loguru
+import sys
+from loguru import logger
+
+logger.remove()
+
+
+logger.add(
+    sys.stderr,
+    format=(
+        "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
+        "<level>{level: <8}</level> | <cyan>{name}</cyan>:"
+        "<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+    ),
+    level="DEBUG",
+)
+base_logger = logger
+
+# logger.debug("This is a debug message.")
+
+# logger.info("This is an info message.")
+
+# logger.success("This is a success message.")
+
+# logger.warning("This is a warning message.")
+
+# logger.error("This is an error message.")
+
+# logger.critical("This is a critical message.")
