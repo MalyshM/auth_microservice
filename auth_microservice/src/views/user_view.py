e3eab3dbc13c3e37fd0a7c5e117148184d37edaf
m.malysh <m.malysh@mail.ocas.local>
2025-01-24 09:39:04 +0500

    WIP: update form_rsp_list and delete_user funcs in `user_view`,

--- auth_microservice/src/views/user_view.py
+++ auth_microservice/src/views/user_view.py
@@ -3,7 +3,6 @@ from sqlmodel import select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from auth_microservice.src.dynamic_models import (
     ID_FIELD,
-    PASSWORD_FIELD,
     UserBase,
     UserBaseType,
     UserDBType,
@@ -21,7 +20,11 @@ class UserView:
         rsp_list = []
         for item in result:
             rsp_list.append(
-                UserPublicDBType(**item.model_dump(exclude_none=True))
+                UserPublicDBType(
+                    **item.model_dump(exclude_none=True, serialize_as_any=True)
+                ).model_dump(
+                    exclude_none=True, serialize_as_any=True, mode="json"
+                )
             )
         return rsp_list
 
@@ -67,7 +70,9 @@ class UserView:
         return await UserView.form_rsp_list(result)
 
     @classmethod
-    async def delete_user(cls, user_id: str, session: AsyncSession) -> dict:
+    async def delete_user(
+        cls, user_id: str, session: AsyncSession
+    ) -> list[dict]:
         try:
             async with session.begin():
                 await session.execute(
@@ -75,7 +80,7 @@ class UserView:
                         getattr(UserDBType, ID_FIELD) == user_id
                     )
                 )
-            return {"status": "success"}
+            return [{"status": "success"}]
         except IntegrityError as e:
             await session.rollback()
             raise HTTPException(
