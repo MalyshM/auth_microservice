8ced8c0d7bc098a3e25ca60e8cf082a6c1ac5767
m.malysh <m.malysh@mail.ocas.local>
2025-01-27 12:09:09 +0500

    WIP rename auth to `reg_log_router.py`, move auth to `auth_router.py`, add get_my_user func logic

--- auth_microservice/src/routers/auth_router.py
+++ auth_microservice/src/routers/auth_router.py
@@ -1,78 +1,15 @@
-from fastapi import APIRouter, Depends, HTTPException, Request, Response
+from fastapi import APIRouter, HTTPException, Request, Response
 from fastapi.responses import JSONResponse
-from sqlalchemy.ext.asyncio import AsyncSession
-from auth_microservice.src.connection import connect_db_data
-from sqlmodel import select
-from sqlalchemy.exc import IntegrityError
 
-from auth_microservice.src.dynamic_models import (
-    ID_FIELD,
-    PASSWORD_FIELD,
-    UserPublicType,
-    UserPublicDBType,
-    UserCreateType,
-    UserDBType,
-)
+from auth_microservice.src.dynamic_models import ID_FIELD
 from auth_microservice.src.token_utils import (
     refresh_access_token,
-    set_refresh_token_cookie,
     verify_access_token,
     verify_refresh_token,
 )
 
+
 auth_router = APIRouter(tags=["Auth"])
-
-
-@auth_router.post(
-    "/register",
-    response_model=UserPublicType,
-)
-async def register_user(
-    user: UserCreateType,
-    session: AsyncSession = Depends(connect_db_data),
-) -> UserPublicType:
-    try:
-        async with session.begin():
-            db_user = UserDBType(**user.model_dump())
-            session.add(db_user)
-    except IntegrityError as e:
-        await session.rollback()
-        raise HTTPException(
-            status_code=400, detail=f"User  could not be registered. {e}"
-        )
-    return await login_user(user, session)
-
-
-@auth_router.post(
-    "/login",
-    response_model=UserPublicType,
-)
-async def login_user(
-    user: UserCreateType,
-    session: AsyncSession = Depends(connect_db_data),
-) -> UserPublicType:
-    try:
-        field, text = user.get_valid_field
-        result = await session.execute(
-            select(UserDBType).where(getattr(UserDBType, field) == text)
-        )
-        result = result.scalars().one()
-        if getattr(result, PASSWORD_FIELD) != getattr(user, PASSWORD_FIELD):
-            raise HTTPException(
-                status_code=403, detail="User provided incorrect data."
-            )
-        rsp_body = UserPublicDBType(**result.model_dump())
-    except IntegrityError as e:
-        raise HTTPException(status_code=404, detail=f"User not found. {e}")
-    except HTTPException as e:
-        raise e
-    except BaseException as e:
-        raise HTTPException(status_code=400, detail=f"Error: {e}")
-    model_dict = rsp_body.model_dump(exclude_none=True)
-    model_dict[ID_FIELD] = str(model_dict[ID_FIELD])
-    response = JSONResponse(content=model_dict, status_code=200)
-    set_refresh_token_cookie(response, rsp_body)
-    return response
 
 
 @auth_router.get(
