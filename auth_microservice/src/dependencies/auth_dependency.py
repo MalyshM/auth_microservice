fba4599c4aefa2223fe97da49800d1f28b19e334
m.malysh <m.malysh@mail.ocas.local>
2025-01-27 12:07:00 +0500

    create `auth_dependency.py`

---
+++ auth_microservice/src/dependencies/auth_dependency.py
@@ -0,0 +1,13 @@
+from typing import Optional
+from fastapi import HTTPException, Request
+from auth_microservice.src.logger import base_logger
+from auth_microservice.src.routers.auth_router import auth
+
+
+async def auth_dependency(request: Request) -> Optional[str]:
+    base_logger.info("Auth dependency")
+    try:
+        json_resp = await auth(request)
+        return json_resp.headers.get("Set-Cookie")
+    except Exception as e:
+        raise HTTPException(status_code=401, detail=f"Error: {e}")
