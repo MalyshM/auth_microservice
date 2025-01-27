67ffb1615434ebbe5de29b38f1b03351bfe5dd7d
m.malysh <m.malysh@mail.ocas.local>
2025-01-27 16:02:00 +0500

    WIP: create `dynamic_db_models.py` for fix tests error, move models to models dir

---
+++ auth_microservice/src/models/dynamic_db_models.py
@@ -0,0 +1,34 @@
+import uuid
+
+from pydantic import create_model, model_validator
+from auth_microservice.src.models.dynamic_models import (
+    ID_FIELD,
+    TABLE_NAME,
+    validate_from_db,
+    UserCreateType,
+)
+from sqlmodel import Field
+
+field_definitions = {}
+validators = {}
+field_definitions[ID_FIELD] = (
+    uuid.UUID,
+    Field(default_factory=uuid.uuid4, primary_key=True),
+)
+field_definitions["__tablename__"] = (str, TABLE_NAME)
+# UserType = create_model(
+#     "User",
+#     __base__=UserCreateType,
+#     __validators__=validators,
+#     **field_definitions,
+#     __cls_kwargs__={"table": True},
+# )
+validators["validate"] = model_validator(mode="before")(validate_from_db)
+
+UserDBType = create_model(
+    "User",
+    __base__=UserCreateType,
+    __validators__=validators,
+    **field_definitions,
+    __cls_kwargs__={"table": True},
+)
--- auth_microservice/src/models/dynamic_models.py
+++ auth_microservice/src/models/dynamic_models.py
@@ -211,27 +211,3 @@ UserBaseDBType = create_model(
     __validators__=validators,
     **field_definitions,
 )
-
-field_definitions = {}
-validators = {}
-field_definitions[ID_FIELD] = (
-    uuid.UUID,
-    Field(default_factory=uuid.uuid4, primary_key=True),
-)
-field_definitions["__tablename__"] = (str, TABLE_NAME)
-# UserType = create_model(
-#     "User",
-#     __base__=UserCreateType,
-#     __validators__=validators,
-#     **field_definitions,
-#     __cls_kwargs__={"table": True},
-# )
-validators["validate"] = model_validator(mode="before")(validate_from_db)
-
-UserDBType = create_model(
-    "User",
-    __base__=UserCreateType,
-    __validators__=validators,
-    **field_definitions,
-    __cls_kwargs__={"table": True},
-)
