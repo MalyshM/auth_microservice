dedda3d532e01b94579ba17d7c3dc1f5dd3d8c09
m.malysh <m.malysh@mail.ocas.local>
2025-01-28 10:09:28 +0500

    update `test_auth.py` tests to remove tests warnings

--- auth_microservice/tests/test_auth.py
+++ auth_microservice/tests/test_auth.py
@@ -39,42 +39,49 @@ def generate_expired_refresh_token():
     return create_refresh_token(DATA, expiration_days=-1)
 
 
+def test_auth_with_no_tokens():
+    response = client.get("/auth")
+    assert response.status_code == 401
+    assert "Not authenticated" in response.json()["detail"]
+
+
 def test_auth_with_valid_access_token():
-    client.cookies.clear()
     access_token = generate_valid_access_token()
-    response = client.get("/auth", cookies={"access_token": access_token})
+    client.cookies.set("access_token", access_token)
+    response = client.get("/auth")
     assert response.status_code == 200
     assert response.json() == TEST_USER_ID
+
+
+def test_auth_with_only_refresh_token():
+    client.cookies.clear()
+    refresh_token = generate_valid_refresh_token()
+    client.cookies.set("refresh_token", refresh_token)
+    response = client.get("/auth")
+    assert response.status_code == 200
+    assert response.json() == TEST_USER_ID
+    assert "access_token" in response.cookies
 
 
 def test_auth_with_expired_access_token_and_valid_refresh_token():
     client.cookies.clear()
     access_token = generate_expired_access_token()
     refresh_token = generate_valid_refresh_token()
-    response = client.get(
-        "/auth",
-        cookies={"access_token": access_token, "refresh_token": refresh_token},
-    )
+    client.cookies.set("refresh_token", refresh_token)
+    client.cookies.set("access_token", access_token)
+    response = client.get("/auth")
     assert response.status_code == 200
     assert response.json() == TEST_USER_ID
     assert "access_token" in response.cookies
-
-
-def test_auth_with_no_tokens():
-    client.cookies.clear()
-    response = client.get("/auth")
-    assert response.status_code == 401
-    assert "Not authenticated" in response.json()["detail"]
 
 
 def test_auth_with_invalid_refresh_token():
     client.cookies.clear()
     access_token = generate_expired_access_token()
     refresh_token = "invalid_refresh_token"
-    response = client.get(
-        "/auth",
-        cookies={"access_token": access_token, "refresh_token": refresh_token},
-    )
+    client.cookies.set("refresh_token", refresh_token)
+    client.cookies.set("access_token", access_token)
+    response = client.get("/auth")
     assert response.status_code == 401
     assert "Not authenticated" in response.json()["detail"]
 
@@ -83,18 +90,8 @@ def test_auth_with_expired_refresh_token():
     client.cookies.clear()
     access_token = generate_expired_access_token()
     refresh_token = generate_expired_refresh_token()
-    response = client.get(
-        "/auth",
-        cookies={"access_token": access_token, "refresh_token": refresh_token},
-    )
+    client.cookies.set("refresh_token", refresh_token)
+    client.cookies.set("access_token", access_token)
+    response = client.get("/auth")
     assert response.status_code == 401
     assert "Not authenticated" in response.json()["detail"]
-
-
-def test_auth_with_only_refresh_token():
-    client.cookies.clear()
-    refresh_token = generate_valid_refresh_token()
-    response = client.get("/auth", cookies={"refresh_token": refresh_token})
-    assert response.status_code == 200
-    assert response.json() == TEST_USER_ID
-    assert "access_token" in response.cookies
