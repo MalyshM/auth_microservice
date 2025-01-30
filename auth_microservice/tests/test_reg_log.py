743a763aad508525e4b3361ccfca0325413664ff
m.malysh <m.malysh@mail.ocas.local>
2025-01-30 11:05:44 +0500

    finish tests in `test_reg_log.py`

--- auth_microservice/tests/test_reg_log.py
+++ auth_microservice/tests/test_reg_log.py
@@ -5,6 +5,43 @@ TEST_USER = {
     "email": "test@example.com",
     "password": "asdASD123!@#",
 }
+TEST_USER_NOT_EXISTS = {
+    "email": "testtest@example.com",
+    "password": "asdASD123!@#",
+}
+
+
+@pytest.mark.asyncio
+async def test_register_existing_user(
+    client: TestClient, override_db_dependency
+):
+    assert client.cookies.get("refresh_token") is None
+    client.post("/register", json=TEST_USER)
+    assert client.cookies.get("refresh_token")
+    client.cookies.delete("refresh_token")
+    response = client.post("/register", json=TEST_USER)
+    assert response.status_code == 200
+    assert client.cookies.get("refresh_token")
+    rsp = response.json()
+    assert isinstance(rsp, list)
+    assert isinstance(rsp[0], dict)
+    assert rsp[0]["email"] == TEST_USER["email"]
+
+
+@pytest.mark.asyncio
+async def test_register_existing_user_wrong_password(
+    client: TestClient, override_db_dependency
+):
+    assert client.cookies.get("refresh_token") is None
+    client.post("/register", json=TEST_USER)
+    assert client.cookies.get("refresh_token")
+    client.cookies.delete("refresh_token")
+    incorrect_credentials = {**TEST_USER, "password": "asdASD123!@#wrong"}
+    response = client.post("/register", json=incorrect_credentials)
+    assert response.status_code == 400
+    rsp = response.json()
+    assert isinstance(rsp, dict)
+    assert "could not be registered" in rsp["detail"]
 
 
 @pytest.mark.asyncio
@@ -12,7 +49,10 @@ async def test_register_user(client: TestClient, override_db_dependency):
     response = client.post("/register", json=TEST_USER)
     assert response.status_code == 200
     assert client.cookies.get("refresh_token")
-    assert response.json()["email"] == TEST_USER["email"]
+    rsp = response.json()
+    assert isinstance(rsp, list)
+    assert isinstance(rsp[0], dict)
+    assert rsp[0]["email"] == TEST_USER["email"]
 
 
 @pytest.mark.asyncio
@@ -32,51 +72,74 @@ async def test_register_duplicate_user(
     assert rsp[0]["email"] == TEST_USER["email"]
 
 
-# Test successful user login
-# @pytest.mark.asyncio
-# async def test_login_user(client: TestClient, override_db_dependency):
-#     # Register the user first
-#     client.post("/register", json=TEST_USER)
-
-#     # Login with the same credentials
-#     response = client.post("/login", json=TEST_USER)
-#     assert response.status_code == 200
-#     assert response.json()["email"] == TEST_USER["email"]
-
-
-# # Test login with incorrect password
-# @pytest.mark.asyncio
-# async def test_login_incorrect_password(client: TestClient, override_db_dependency):
-#     # Register the user first
-#     client.post("/register", json=TEST_USER)
-
-#     # Login with incorrect password
-#     incorrect_credentials = {**TEST_USER, "password": "wrongpassword"}
-#     response = client.post("/login", json=incorrect_credentials)
-#     assert response.status_code == 403
-#     assert "incorrect data" in response.json()["detail"]
+@pytest.mark.asyncio
+async def test_login_user(client: TestClient, override_db_dependency):
+    assert client.cookies.get("refresh_token") is None
+    client.post("/register", json=TEST_USER)
+    assert client.cookies.get("refresh_token")
+    response = client.post("/login", json=TEST_USER)
+    assert response.status_code == 200
+    assert client.cookies.get("refresh_token")
+    assert client.cookies.get("access_token")
+    rsp = response.json()
+    assert isinstance(rsp, list)
+    assert isinstance(rsp[0], dict)
+    assert rsp[0]["email"] == TEST_USER["email"]
 
 
-# # Test login with non-existent user
-# @pytest.mark.asyncio
-# async def test_login_nonexistent_user(client: TestClient, override_db_dependency):
-#     # Try to login with a user that doesn't exist
-#     response = client.post("/login", json=TEST_USER)
-#     assert response.status_code == 404
-#     assert "not found" in response.json()["detail"]
+@pytest.mark.asyncio
+async def test_login_incorrect_password_with_refresh_token(
+    client: TestClient, override_db_dependency
+):
+    assert client.cookies.get("refresh_token") is None
+    client.post("/register", json=TEST_USER)
+    assert client.cookies.get("refresh_token")
+    incorrect_credentials = {**TEST_USER, "password": "asdASD123!@#wrong"}
+    response = client.post("/login", json=incorrect_credentials)
+    assert response.status_code == 200
+    rsp = response.json()
+    assert isinstance(rsp, list)
+    assert isinstance(rsp[0], dict)
+    assert rsp[0]["email"] == TEST_USER["email"]
 
 
-# # Test database error during registration
-# @pytest.mark.asyncio
-# async def test_register_database_error(
-#     client: TestClient, override_db_dependency, async_session
-# ):
-#     # Simulate a database error
-#     async with async_session() as session:
-#         session.add(UserDBType(**TEST_USER))
-#         await session.commit()
+@pytest.mark.asyncio
+async def test_login_incorrect_password(
+    client: TestClient, override_db_dependency
+):
+    assert client.cookies.get("refresh_token") is None
+    client.post("/register", json=TEST_USER)
+    assert client.cookies.get("refresh_token")
+    client.cookies.delete("refresh_token")
+    incorrect_credentials = {**TEST_USER, "password": "asdASD123!@#wrong"}
+    response = client.post("/login", json=incorrect_credentials)
+    assert response.status_code == 403
+    rsp = response.json()
+    assert isinstance(rsp, dict)
+    assert "incorrect" in rsp["detail"]
 
-#     # Try to register the same user again
-#     response = client.post("/register", json=TEST_USER)
-#     assert response.status_code == 400
-#     assert "could not be registered" in response.json()["detail"]
+
+@pytest.mark.asyncio
+async def test_login_correct_password(
+    client: TestClient, override_db_dependency
+):
+    assert client.cookies.get("refresh_token") is None
+    client.post("/register", json=TEST_USER)
+    assert client.cookies.get("refresh_token")
+    client.cookies.delete("refresh_token")
+    response = client.post("/login", json=TEST_USER)
+    assert response.status_code == 200
+    rsp = response.json()
+    assert isinstance(rsp, list)
+    assert isinstance(rsp[0], dict)
+    assert rsp[0]["email"] == TEST_USER["email"]
+
+
+@pytest.mark.asyncio
+async def test_login_incorrect_user(client: TestClient, override_db_dependency):
+    assert client.cookies.get("refresh_token") is None
+    response = client.post("/login", json=TEST_USER_NOT_EXISTS)
+    assert response.status_code == 404
+    rsp = response.json()
+    assert isinstance(rsp, dict)
+    assert "User not found" in rsp["detail"]
