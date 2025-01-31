2c0abfce157ecd7951935cf105c9b6d63034f910
m.malysh <m.malysh@mail.ocas.local>
2025-01-31 17:24:54 +0500

    add docs to `auth_router.py`

--- auth_microservice/src/routers/auth_router.py
+++ auth_microservice/src/routers/auth_router.py
@@ -7,17 +7,45 @@ from ..token_utils import (
     verify_access_token,
     verify_refresh_token,
 )
-
+from ..docs.responses import auth_200, response_401
 
 auth_router = APIRouter(tags=["Auth"])
 
 
 @auth_router.get(
     "/auth",
+    summary="Authenticate User",
+    description=(
+        "Authenticates the user by verifying the access token or "
+        "refreshing it using the refresh token."
+    ),
+    responses={
+        200: auth_200,
+        401: response_401,
+    },
 )
 async def auth(
     request: Request,
 ) -> Response:
+    """
+    Authenticates the user by checking the access token or refreshing it using
+    the refresh token.
+
+    ### Steps:
+    1. Checks if the `access_token` is present in the cookies and validates it.
+    2. If the `access_token` is invalid or expired, it checks for a
+    `refresh_token`.
+    3. If the `refresh_token` is valid, a new `access_token` is generated
+    and returned.
+    4. If neither token is valid, an HTTP 401 Unauthorized error is returned.
+
+    ### Parameters:
+    - **request**: The incoming HTTP request containing cookies.
+
+    ### Returns:
+    - **JSONResponse**: The user GUID if authentication is successful.
+    - **HTTPException**: 401 Unauthorized if authentication fails.
+    """
     cookies = request.cookies
     payload = (
         verify_access_token(cookies["access_token"])
