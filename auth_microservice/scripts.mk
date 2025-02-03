3b5bd9b337d61b81f7e5e46a50c5e7ea3e0caf29
m.malysh <m.malysh@mail.ocas.local>
2025-02-03 10:54:13 +0500

    add dependency-groups and tools settings in `pyproject.toml`, update `uv.lock`, add check and format commands in `scripts.mk`, delete double use of black in `ruff_mypy.sh`

--- auth_microservice/scripts.mk
+++ auth_microservice/scripts.mk
@@ -2,5 +2,11 @@
 test:
 	./auth_microservice/scripts/test.sh
 
+check: 
+	./auth_microservice/scripts/ruff_mypy.sh
+
+format: 
+	./auth_microservice/scripts/format.sh
+
 start:
 	uvicorn auth_microservice.src.main:app --host 0.0.0.0 --port 8090 --reload --forwarded-allow-ips='*' --proxy-headers
