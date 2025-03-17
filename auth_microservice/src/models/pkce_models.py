c7b26b8a408d648dcdc0f3c006b7bf1e32ea8616
m.malysh <m.malysh@mail.ocas.local>
2025-03-17 16:31:56 +0500

    create `pkce_models.py`, `pkce_sheme.py`

---
+++ auth_microservice/src/models/pkce_models.py
@@ -0,0 +1,50 @@
+import uuid
+from pydantic import create_model
+from sqlmodel import Field, SQLModel
+
+from ..models.dynamic_models import ID_FIELD
+
+
+class PKCE(SQLModel):
+
+    host: str = Field(
+        default=None,
+        unique=True,
+        title="Host",
+        description=(
+            "The host associated with the PKCE request. Must "
+            "be unique across all entries."
+        ),
+    )
+    code_challenge: str = Field(
+        default=None,
+        max_length=255,
+        title="Code Challenge",
+        description=(
+            "The code challenge used in the PKCE flow. Must "
+            "be a valid string representation of the challenge."
+        ),
+    )
+    code_challenge_method: str = Field(
+        default=None,
+        max_length=255,
+        title="Code Challenge Method",
+        description=(
+            "The method used to generate the code challenge. "
+            "Common methods include 'S256' for SHA-256."
+        ),
+    )
+
+
+field_definitions = {}
+field_definitions[ID_FIELD] = (
+    uuid.UUID,
+    Field(default_factory=uuid.uuid4, primary_key=True),
+)
+field_definitions["__tablename__"] = (str, "pkce")
+PKCEType = create_model(
+    "PKCE",
+    __base__=PKCE,
+    **field_definitions,
+    __cls_kwargs__={"table": True},
+)
---
+++ auth_microservice/src/schemes/pkce_sheme.py
@@ -0,0 +1,57 @@
+from typing import Optional
+from pydantic import BaseModel, Field, model_validator
+
+
+class PKCE_scheme(BaseModel):
+
+    code_verifier: Optional[str] = Field(
+        default=None,
+        title="Code Verifier",
+        description=(
+            "A string that serves as the code verifier. "
+            "It is used to ensure that the authorization "
+            "code was not intercepted."
+        ),
+    )
+    code_challenge: Optional[str] = Field(
+        default=None,
+        title="Code Challenge",
+        description=(
+            "A string that represents the code challenge. "
+            "It is derived from the code verifier and is "
+            "sent to the authorization server."
+        ),
+    )
+    code_challenge_method: Optional[str] = Field(
+        default=None,
+        title="Code Challenge Method",
+        description=(
+            "A string indicating the method used to generate "
+            "the code challenge. "
+            "Common methods include 'plain' and 'S256' for SHA-256."
+        ),
+    )
+
+    @model_validator(mode="before")
+    @classmethod
+    def pkce_validate(cls, dict_values: dict) -> dict:
+
+        if (
+            "code_verifier" in dict_values
+            and "code_challenge" not in dict_values
+            and "code_challenge_method" not in dict_values
+        ):
+            return dict_values
+        elif (
+            "code_verifier" not in dict_values
+            and "code_challenge" in dict_values
+            and "code_challenge_method" in dict_values
+        ):
+            return dict_values
+        raise ValueError(
+            "You can provide either code_verifier or "
+            "code_challenge and code_challenge_method"
+        )
+
+    def is_to_check(self) -> bool:
+        return bool(self.code_verifier)
