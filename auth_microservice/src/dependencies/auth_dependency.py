597d7e54e7a4723aa14a9377c12b4e3272fd1992
m.malysh <m.malysh@mail.ocas.local>
2025-03-17 16:30:49 +0500

    update `auth_dependency.py`, implement it in `user_router.py`

--- auth_microservice/src/dependencies/auth_dependency.py
+++ auth_microservice/src/dependencies/auth_dependency.py
@@ -1,15 +1,20 @@
-from typing import Optional
-
 from fastapi import HTTPException, Request
 
+from ..token_utils import verify_access_token
+
 from ..logger import base_logger
-from ..routers.auth_router import auth
 
 
-async def auth_dependency(request: Request) -> Optional[str]:
+async def auth_dependency(request: Request) -> bool:
     base_logger.info("Auth dependency")
     try:
-        json_resp = await auth(request)
-        return json_resp.headers.get("Set-Cookie")
+        # json_resp = await auth(request)
+        cookies = request.cookies
+        payload = (
+            verify_access_token(cookies["access_token"])
+            if cookies.get("access_token")
+            else None
+        )
+        return bool(payload)
     except Exception as e:
         raise HTTPException(status_code=401, detail=f"Error: {e}")
